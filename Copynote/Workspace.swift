41168d18d8c229f889090746052ab76199578b96
송영모 <mooyoung2309@gmail.com>
2022-12-30 00:22:27 +0900

    프로젝트 세팅

---
+++ Copynote/Workspace.swift
@@ -0,0 +1,12 @@
+//
+//  Workspace.swift
+//  ProjectDescriptionHelpers
+//
+//  Created by 송영모 on 2022/12/29.
+//
+
+import ProjectDescription
+
+let appName = "Copynote"
+
+let workspace = Workspace(name: appName, projects: ["Projects/*"])
