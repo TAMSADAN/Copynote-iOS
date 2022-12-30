e75b9fddfc8484217a0cbb1fa58fcd6def43d751
송영모 <mooyoung2309@gmail.com>
2022-12-30 23:22:05 +0900

    [Feat] #5 - 입력창 추가

--- Copynote/Projects/Copynote/Sources/Presenter/CreateNote/CreateNoteViewController.swift
+++ Copynote/Projects/Copynote/Sources/Presenter/CreateNote/CreateNoteViewController.swift
@@ -18,6 +18,12 @@ class CreateNoteViewController: NavigationViewController, View {
 
     // MARK: - UI Components
     
+    let kindButton: UIButton = .init(type: .system)
+    let categoryButton: UIButton = .init(type: .system)
+    let divider: UIView = .init()
+    let titleTextField: UITextField = .init()
+    let contentTextView: UITextView = .init()
+    
     init(mode: PresentMode, reactor: Reactor) {
         self.mode = mode
         super.init(nibName: nil, bundle: nil)
@@ -33,6 +39,64 @@ class CreateNoteViewController: NavigationViewController, View {
         super.setupNavigationBar()
         
         showNavigtaionBar(isHidden: false)
+    }
+    
+    override func setupProperty() {
+        super.setupProperty()
+        
+        kindButton.setTitle("태그: Memo", for: .normal)
+        kindButton.setTitleColor(.black, for: .normal)
+        kindButton.tintColor = .black
+        kindButton.titleLabel?.font = CopynoteFontFamily.HappinessSansPrint.regular.font(size: 16)
+        
+        categoryButton.setTitle("카테고리: 테스트", for: .normal)
+        categoryButton.setTitleColor(.black, for: .normal)
+        categoryButton.tintColor = .black
+        categoryButton.titleLabel?.font = CopynoteFontFamily.HappinessSansPrint.regular.font(size: 16)
+        
+        divider.backgroundColor = .black
+        
+        titleTextField.placeholder = "제목을 입력하세요."
+        titleTextField.font = CopynoteFontFamily.HappinessSansPrint.title.font(size: 30)
+        
+        contentTextView.font = CopynoteFontFamily.HappinessSansPrint.regular.font(size: 17)
+    }
+    
+    override func setupHierarchy() {
+        super.setupHierarchy()
+        
+        contentView.addSubviews([kindButton, categoryButton, divider, titleTextField, contentTextView])
+    }
+    
+    override func setupLayout() {
+        super.setupLayout()
+        
+        kindButton.snp.makeConstraints {
+            $0.top.equalToSuperview()
+            $0.leading.equalToSuperview().inset(20)
+        }
+        
+        categoryButton.snp.makeConstraints {
+            $0.top.equalTo(kindButton.snp.bottom)
+            $0.leading.equalToSuperview().inset(20)
+        }
+        
+        divider.snp.makeConstraints {
+            $0.top.equalTo(categoryButton.snp.bottom).offset(10)
+            $0.leading.trailing.equalToSuperview().inset(20)
+            $0.height.equalTo(1)
+        }
+        
+        titleTextField.snp.makeConstraints {
+            $0.top.equalTo(divider.snp.bottom).offset(15)
+            $0.leading.trailing.equalToSuperview().inset(20)
+        }
+        
+        contentTextView.snp.makeConstraints {
+            $0.top.equalTo(titleTextField.snp.bottom)
+            $0.leading.trailing.equalToSuperview().inset(20)
+            $0.bottom.equalToSuperview()
+        }
     }
     
     func bind(reactor: Reactor) {
